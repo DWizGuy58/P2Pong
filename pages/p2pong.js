773a64d372afc1151e2ad798ec725059ad50b02d
nweinshenker <nweinshenker@wisc.edu>
2020-08-27 14:10:03 -0500

    Implemented Rotate cube

--- pages/p2pong.js
+++ pages/p2pong.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Board from "../components/Board"
+import CubePiece from "../components/CubePiece"
 
 export default function Home() {
   const numPlayers = 3;
   return (
-    <Board numPlayers= {numPlayers}/>
+    //<Board numPlayers= {numPlayers}/>
+    <CubePiece />
   )
 }
