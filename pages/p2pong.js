700416b4fa1eb2a69322d26995e323ac535026a0
Daniel <weinshenkerdaniel@gmail.com>
2020-08-26 20:44:24 -0700

    add basic components

---
+++ pages/p2pong.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+      <canvas style={{width: 500, height: 500, border: "1px solid black", margin: 5}}>
+          
+      </canvas>
+  )
+}
