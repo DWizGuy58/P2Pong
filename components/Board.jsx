1ad05912c882ee8ea32857f193e8840827a59814
Daniel <weinshenkerdaniel@gmail.com>
2020-10-02 21:14:13 -0700

    update react component to initialize Game

--- components/Board.jsx
+++ components/Board.jsx
@@ -1,73 +1,15 @@
 import {useRef, useEffect} from 'react'
-import { handleKeyDown, handleUserClick } from '../eventlistners/userinput';
+//import { handleKeyDown, handleUserClick } from '../eventlistners/userinput';
+import Game from '../src/Game';
 
 const Board = ({ numPlayers }) => {
     const canvasRef = useRef(null);
-    let width, height;
-    const sideLength = 50;
-    const angleInRadians = (Math.PI * 2) / numPlayers;
-
-    const drawGame = () => {
-        width = canvasRef.current.width;
-        height = canvasRef.current.height;
-        const ctx = canvasRef.current.getContext("2d");
-        //ctx.clearRect(0, 0, width, height);
-        ctx.save();
-        ctx.translate(width / 2,height / 2);
-        drawBoard(ctx);
-        for (let playerNumber = 0; playerNumber < numPlayers; playerNumber++) {
-            drawPlayer(ctx, playerNumber);
-        }
-        ctx.restore();
-        //requestAnimationFrame(drawGame);
-    }
-
-    const drawBoard = (ctx) => {            
-        ctx.beginPath();
-        for (var i = 1; i <= numPlayers; i++) {
-            ctx.lineTo(sideLength*Math.cos(angleInRadians*i),sideLength*Math.sin(angleInRadians*i));
-        }
-        ctx.closePath();
-        ctx.stroke();
-    }
-    
-    const drawPoint = (ctx) => {
-        ctx.beginPath();
-        ctx.arc(2, 1, 1, 0, 2 * Math.PI, true);
-        ctx.stroke();
-    }
-
-    const drawPlayer = (ctx, playerNumber) => {
-        ctx.strokeStyle = '#ff0000';
-        const playerSideLength = sideLength * 0.9;
-        //ctx.lineTo(sideLength*Math.cos(angleInRadians*i),sideLength*Math.sin(angleInRadians*i))
-
-        const x1 = playerSideLength * Math.cos(angleInRadians * playerNumber);
-        const y1 = playerSideLength * Math.sin(angleInRadians * playerNumber);
-
-        const x2 = playerSideLength * Math.cos(angleInRadians * (playerNumber + 1));
-        const y2 = playerSideLength * Math.sin(angleInRadians * (playerNumber + 1));        
-        console.log(playerNumber, x1, y1, x2, y2);
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-
-        const slope = (y2 - y1) / (x2 - x1);
-        const x3 = x1 + ((x2 - x1) * 0.5);
-        const y3 = y1 + ((y2 - y1) * 0.5);
-        //ctx.moveTo(x3, y3);
-
-
-
-        //ctx.moveTo(vertex1X, vertex1Y);
-        ctx.lineTo(x3, y3);
-        ctx.stroke();
-    }
-
 
     useEffect(() => {
-        document.addEventListener("keydown", handleKeyDown);
-        if (canvasRef.current !== null) {     
-            requestAnimationFrame(drawGame);
+        //document.addEventListener("keydown", handleKeyDown);
+        if (canvasRef.current !== null) {
+            const game = new Game(8, canvasRef.current);     
+            game.draw();
         }
       }, [canvasRef]);
 
